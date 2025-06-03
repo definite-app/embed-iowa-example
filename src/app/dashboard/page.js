fefd948e674d855319a67c45a4145ff35bbad333
Stefan Kovac <mali_stefan@msn.com>
2025-06-03 18:25:11 -0400

    add canvas id and local dev env

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,7 +5,7 @@ import crypto from 'crypto';
 export const fetchCache = 'force-no-store';
 
 export default async function Dashboard() {
-  const canvasId = '0b99601e-8401-4113-bcb8-8488c0e28bcf';
+  const canvasId = '9f257615-e7a5-433e-957f-c64a3b6fdf90';
   // Generate a random 16-byte (128-bit) user identifier
   const userIdentifier = crypto.randomBytes(16).toString('hex');
   const requiredFilters = [
